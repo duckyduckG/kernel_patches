57b6b0911e99c899fb8f94660df82c6ded61212f
duckyduckg <duckyduckg65@gmail.com>
2025-07-12 08:31:39 +0500

    aosp: rework for beryllium
    later dipper and others.
    
    Signed-off-by: duckyduckg <duckyduckg65@gmail.com>

--- aosp/build.sh
+++ aosp/build.sh
@@ -1,71 +1,56 @@
 #!/bin/bash
 
-rm -rf prebuilts/clang/host/linux-x86
-
-# Repo Init
-repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
-
-# Sync the repositories
-/opt/crave/resync.sh
-
 # Remove existing directories
 
-rm -rf device/oneplus/fajita
-rm -rf device/oneplus/sdm845-common
-rm -rf vendor/oneplus/fajita
-rm -rf vendor/oneplus/sdm845-common
-rm -rf kernel/oneplus/sdm845
-rm -rf hardware/oneplus
+# remove device trees
+rm -rf device/xiaomi/beryllium
+rm -rf device/xiaomi/sdm845-common
+# remove vendor
+rm -rf vendor/xiaomi/beryllium
+rm -rf vendor/xiaomi/sdm845-common
+# remove kernel
+rm -rf kernel/xiaomi/sdm845
+# remove hardware
+rm -rf hardware/xiaomi
+# remove hals
 rm -rf hardware/qcom-caf/sdm845/media
 rm -rf hardware/qcom-caf/sdm845/audio
 rm -rf hardware/qcom-caf/sdm845/display
+# remove sepolicy
 rm -rf device/lineage/sepolicy
 rm -rf device/qcom/sepolicy_vndr/legacy-um
-rm -rf hardware/qcom-caf/bootctrl
-rm -rf external/libhybris
-rm -rf vendor/lindroid
-rm -rf external/lxc
-rm -rf frameworks/native
-rm -rf frameworks/base
 
 # Clone repositories #
 
-# Device
-git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b Evo_16 device/oneplus/fajita
-git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b rebase16-1 device/oneplus/sdm845-common
+# Device tree
+git clone https://github.com/duckyduckG/android_device_xiaomi_beryllium_419.git -b lineage-22.2 device/xiaomi/beryllium
+git clone https://github.com/duckyduckG/android_device_xiaomi_sdm845-common_419.git -b lineage-22.2 device/xiaomi/sdm845-common
 
 # Vendor
-git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.0 vendor/oneplus/fajita
-git clone https://github.com/shinichi-c/proprietary_vendor_oneplus_sdm845-common --depth=1 -b elf_checks vendor/oneplus/sdm845-common
+git clone https://github.com/duckyduckG/android_vendor_xiaomi_beryllium_419.git -b lineage-22.2 vendor/xiaomi/beryllium
+git clone https://github.com/duckyduckG/android_vendor_xiaomi_sdm845-common_419.git -b lineage-22.2 vendor/xiaomi/sdm845-common
 
 # Kernel
-git clone https://github.com/shinichi-c/android_kernel_qcom_sdm845 --depth=1 -b next2-bpf_5.4 kernel/oneplus/sdm845
+git clone https://github.com/duckyduckG/android_kernel_xiaomi_sdm845_419.git -b perf kernel/xiaomi/sdm845
+# kernel techpack
+git clone https://github.com/duckyduckG/kernel_techpack_mi845_419.git kernel/xiaomi/sdm845/techpack/mi845
+# kernel device tree
+git clone https://github.com/duckyduckG/devicetree_xiaomi_419.git kernel/xiaomi/sdm845/arch/arm64/boot/dts/xiaomi
+git clone https://github.com/duckyduckG/devicetree_qcom-sdm845_419.git kernel/xiaomi/sdm845/arch/arm64/boot/dts/qcom-sdm845
 
 # Hardware
-git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b new hardware/oneplus
+git clone https://github.com/duckyduckG/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
 
 # Hals
-git clone https://github.com/shinichi-c/media --depth=1 -b upstream hardware/qcom-caf/sdm845/media
-git clone https://github.com/shinichi-c/audio --depth=1 -b update hardware/qcom-caf/sdm845/audio
-git clone https://github.com/shinichi-c/display --depth=1 -b upstream hardware/qcom-caf/sdm845/display
+git clone https://github.com/duckyduckG/media.git hardware/qcom-caf/sdm845/media
+git clone https://github.com/duckyduckG/audio.git hardware/qcom-caf/sdm845/audio
+git clone https://github.com/duckyduckG/display.git hardware/qcom-caf/sdm845/display
 
 # Sepolicy
-git clone https://github.com/shinichi-c/device_evolution_sepolicy --depth=1 -b bka2 device/lineage/sepolicy
-git clone https://github.com/shinichi-c/device_qcom_sepolicy_vndr_evo --depth=1 -b bka-legacy-um device/qcom/sepolicy_vndr/legacy-um
-
-# bootctrl
-git clone https://github.com/shinichi-c/android_hardware_qcom_bootctrl --depth=1 -b lineage-22.2-caf hardware/qcom-caf/bootctrl
-
-# framework
-git clone https://github.com/shinichi-c/frameworks_native_evo16 --depth=1 -b bka frameworks/native
-git clone https://github.com/shinichi-c/frameworks_base_evo --depth=1 -b bka frameworks/base
-
-# lindroid
-git clone https://github.com/Linux-on-droid/libhybris --depth=1 -b tmp external/libhybris
-git clone https://github.com/shinichi-c/vendor_lindroid --depth=1 -b lindroid-22.1 vendor/lindroid
-git clone https://github.com/Linux-on-droid/external_lxc --depth=1 -b lindroid-21 external/lxc
+git clone https://github.com/duckyduckG/android_device_lineage_sepolicy.git -b lineage-22.2 device/lineage/sepolicy
+git clone https://github.com/duckyduckG/android_device_qcom_sepolicy_vndr.git -b lineage-22.2-legacy-um device/qcom/sepolicy_vndr/legacy-um
 
 #lunch
 source build/envsetup.sh
-lunch lineage_fajita-bp2a-userdebug
-m evolution
+lunch lineage_beryllium-bp2a-userdebug
+m lineage
