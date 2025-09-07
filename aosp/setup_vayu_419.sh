32d075e992e461956afc58f2a638cf78b0d64239
duckyduckG <duckyduckg65@gmail.com>
2025-09-07 21:53:13 +0500

    aosp: add vayu setup script

---
+++ aosp/setup_vayu_419.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# remove device trees
+rm -rf device/xiaomi/vayu
+rm -rf device/xiaomi/sm8150-common
+# remove vendor
+rm -rf vendor/xiaomi/vayu
+rm -rf vendor/xiaomi/sm8150-common
+# remove kernel
+rm -rf kernel/xiaomi/sm8150
+# remove hardware
+rm -rf hardware/xiaomi
+# Clone repositories #
+
+# Device tree - vayu
+git clone https://github.com/duckyduckG/android_device_xiaomi_vayu_419.git -b lineage-22.2 device/xiaomi/vayu
+# Device tree - sm8150-common
+git clone https://github.com/duckyduckG/android_device_xiaomi_sm8150-common_419.git -b lineage-22.2 device/xiaomi/sm8150-common
+
+# Vendor - vayu
+git clone https://github.com/duckyduckG/proprietary_vendor_xiaomi_vayu_419.git -b lineage-22.2 vendor/xiaomi/vayu
+# Vendor - sm8150-common
+git clone https://github.com/duckyduckG/proprietary_vendor_xiaomi_sm8150-common_419.git -b lineage-22.2 vendor/xiaomi/sm8150-common
+
+# Kernel
+git clone https://github.com/duckyduckG/android_kernel_xiaomi_sm8150_419.git -b SM8150-419-T-AUDIO kernel/xiaomi/sm8150
+# kernel device tree
+git clone https://github.com/duckyduckG/devicetree_xiaomi_sm8150_419.git kernel/xiaomi/sm8150/arch/arm64/boot/dts/xiaomi
+git clone https://github.com/duckyduckG/devicetree_qcom-sm8150_419.git kernel/xiaomi/sm8150/arch/arm64/boot/dts/qcom-sm8150
+
+# Hardware
+git clone https://github.com/duckyduckG/android_hardware_xiaomi.git -b lineage-22.2 hardware/xiaomi
