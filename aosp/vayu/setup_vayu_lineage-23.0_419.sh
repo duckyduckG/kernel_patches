f75c6b406aeca256653b190e565bce7bd9f82b68
duckyduckG <duckyduckg65@gmail.com>
2025-10-27 13:01:48 +0500

    aosp: vayu: los23: update kernel link

--- aosp/vayu/setup_vayu_lineage-23.0_419.sh
+++ aosp/vayu/setup_vayu_lineage-23.0_419.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+# remove old start #
 # remove device trees
 rm -rf device/xiaomi/vayu
 rm -rf device/xiaomi/sm8150-common
@@ -10,13 +11,13 @@ rm -rf vendor/xiaomi/sm8150-common
 rm -rf kernel/xiaomi/sm8150
 # remove hardware
 rm -rf hardware/xiaomi
-# Clone repositories #
-
 # remove hals
 rm -rf hardware/qcom-caf/sm8150/media
 rm -rf hardware/qcom-caf/sm8150/audio
 rm -rf hardware/qcom-caf/sm8150/display
+# remove old end #
 
+# Clone repositories start #
 # Device tree - vayu
 git clone https://github.com/duckyduckG/android_device_xiaomi_vayu_419.git -b lineage-23.0 device/xiaomi/vayu
 # Device tree - sm8150-common
@@ -28,10 +29,7 @@ git clone https://github.com/duckyduckG/proprietary_vendor_xiaomi_vayu_419.git -
 git clone https://github.com/duckyduckG/proprietary_vendor_xiaomi_sm8150-common_419.git -b lineage-23.0 vendor/xiaomi/sm8150-common
 
 # Kernel
-git clone https://github.com/duckyduckG/android_kernel_xiaomi_sm8150_419.git -b lineage-23.0-wip kernel/xiaomi/sm8150
-# kernel device tree
-git clone https://github.com/duckyduckG/devicetree_xiaomi_sm8150_419.git kernel/xiaomi/sm8150/arch/arm64/boot/dts/xiaomi
-git clone https://github.com/duckyduckG/devicetree_qcom-sm8150_419.git kernel/xiaomi/sm8150/arch/arm64/boot/dts/qcom-sm8150
+git clone https://github.com/duckyduckG/android_kernel_xiaomi_sm8150_419.git -b lineage-23.0_vayu kernel/xiaomi/sm8150
 
 # Hardware
 git clone https://github.com/duckyduckG/android_hardware_xiaomi.git -b lineage-23.0 hardware/xiaomi
@@ -42,3 +40,4 @@ git clone https://github.com/duckyduckG/android_hardware_qcom_audio.git -b linea
 git clone https://github.com/duckyduckG/android_hardware_qcom_media.git -b lineage-23.0-caf-sm8150-419 hardware/qcom-caf/sm8150/media
 # display
 git clone https://github.com/duckyduckG/android_hardware_qcom_display.git -b lineage-23.0-caf-sm8250 hardware/qcom-caf/sm8150/display
+# Clone repositories end #
